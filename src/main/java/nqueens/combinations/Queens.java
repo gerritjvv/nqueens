fd2417a56785999d4be0ada382755b5a3547d942
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2020-03-01 17:15:36 +0100

    single array solution

--- src/main/java/nqueens/combinations/Queens.java
+++ src/main/java/nqueens/combinations/Queens.java
@@ -26,38 +26,21 @@ public class Queens {
          * the value at each index is the column of the queen set can be any 0 -> (n-1)
          */
         int[] queens = new int[n];
-        combinations(consumer, queens);
+        combinations(consumer, queens, 0);
     }
 
-    private static final void combinations(Consumer<int[]> consumer, int[] queens) {
+    private static final void combinations(Consumer<int[]> consumer, int[] queens, int row) {
         int n = queens.length;
 
-        int[][] stack = new int[n*10][2];
-        int pointer = 1;
-
-        STACKLOOP:
-        for(; pointer > 0;) {
-
-            int row = stack[pointer][0];
-            int colStart = stack[pointer][1];
-
-            if (n == row) {
-                consumer.accept(copyArray(queens));
-            } else {
-                for (int col = colStart; col < n; col++) {
-                    queens[row] = col; // set the queen position
-                    if (!isOnAttackPath(queens, row)) {
-                        // push to stack
-                        stack[pointer][1] = col + 1;
-                        pointer++;
-                        stack[pointer][0] = row+1;
-                        stack[pointer][1] = 0;
-                        continue STACKLOOP;
-                    }
+        if (n == row) {
+            consumer.accept(copyArray(queens));
+        } else {
+            for (int col = 0; col < n; col++) {
+                queens[row] = col; // set the queen position
+                if (!isOnAttackPath(queens, row)) {
+                    combinations(consumer, queens, row + 1);
                 }
             }
-            pointer--;
-
         }
     }
 
@@ -69,7 +52,6 @@ public class Queens {
 
     public static final void main(String[] args) {
         AtomicInteger counter = new AtomicInteger();
-        long ms = System.currentTimeMillis();
 
         Queens.solve(15, arr -> {
             counter.incrementAndGet();
